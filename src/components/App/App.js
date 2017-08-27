56a989e66d8c726f66385c3d347dbec9d4f00fc5
miri <mirif@wix.com>
2017-08-27 17:39:20 +0300

    added login button

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Input from 'wix-style-react/dist/src/Input';
+import Button from 'wix-style-react/dist/src/Button';
 
 import s from './App.scss';
 
-function App() {
-  return (
-    <div className={s.root}>
-      <div>
-        <Input dataHook="login-username"/>
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: null,
+      isLoggedIn: false
+    };
+  }
+
+  render() {
+    const {username, isLoggedIn} = this.state;
+    return (
+      <div className={s.root}>
+        <div>
+          <Input dataHook="login-username" onChange={evt => this.setState({username: evt.target.value})}/>
+          <Button dataHook="login-btn" onClick={() => this.setState({isLoggedIn: true})}/>
+          {isLoggedIn && <div data-hook="username">{username}</div>}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 App.propTypes = {
--- test/e2e/app.e2e.js
+++ test/e2e/app.e2e.js
@@ -1,15 +1,21 @@
 import 'babel-polyfill';
 import {expect} from 'chai';
 import {beforeAndAfter, app} from './../environment';
-import {inputTestkitFactory, waitForVisibilityOf} from 'wix-style-react/dist/testkit/protractor';
+import {inputTestkitFactory, buttonTestkitFactory, waitForVisibilityOf} from 'wix-style-react/dist/testkit/protractor';
 describe('Wazzap E2E tests', () => {
 
   beforeAndAfter();
-  it('should have a login field', async () => {
+  const user1 = 'Donald';
+  it('should login to app', async () => {
     await browser.get(app.getUrl('/'));
     const userInput = inputTestkitFactory({dataHook: 'login-username'});
     await waitForVisibilityOf(userInput.element(), 'Cannot find Input');
     expect(await userInput.element().isDisplayed()).to.equal(true);
+    await userInput.enterText(user1);
+    const loginButton = buttonTestkitFactory({dataHook: 'login-btn'});
+    expect(await $('[data-hook="username"]').isPresent()).to.be.false;
+    await loginButton.click();
+    expect(await $('[data-hook="username"]').getText()).to.equal(user1);
   });
 
 
