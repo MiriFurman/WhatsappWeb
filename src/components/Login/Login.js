e28c995f00a534644ac3681621b23b81a2455e71
miri <mirif@wix.com>
2017-08-30 17:08:01 +0300

    login css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,6 +5,8 @@ import Button from 'wix-style-react/dist/src/Button';
 import TextField from 'wix-style-react/dist/src/TextField';
 import Text from 'wix-style-react/dist/src/Text';
 import Label from 'wix-style-react/dist/src/Label';
+import Checkbox from 'wix-style-react/dist/src/Checkbox';
+import TextLink from 'wix-style-react/dist/src/TextLink';
 import s from './Login.scss';
 
 
@@ -12,7 +14,7 @@ class Login extends React.Component {
   constructor() {
     super();
 
-    this.state = {username: ''};
+    this.state = {username: '', checked: true};
   }
 
   render() {
@@ -20,11 +22,29 @@ class Login extends React.Component {
       <div className={s.loginBox}>
         <Text appearance="H1">Welcome to Wazzap</Text>
         <Text appearance="T1">Sign in to your account by filling the form bellow.</Text>
-        <TextField>
-          <Label appearance="T1.1" for="username">User Name</Label>
-          <Input id="username" size="normal" maxLength={524288} textOverflow="clip" theme="normal" width="initial" dataHook="login-username" onChange={evt => this.setState({username: evt.target.value})}/>
-        </TextField>
-        <Button dataHook="login-btn" onClick={() => this.props.onLoginClick(this.state.username)}>Login</Button>
+        <div className={s.loginForm}>
+          <div className={s.inputField}>
+            <TextField>
+              <Label appearance="T1.1" for="username">User Name</Label>
+              <Input id="username" size="normal" maxLength={524288} textOverflow="clip" theme="normal" width="initial" dataHook="login-username" onChange={evt => this.setState({username: evt.target.value})}/>
+            </TextField>
+          </div>
+          <div className={s.inputField}>
+            <TextField>
+              <Label appearance="T1.1" for="password">Password</Label>
+              <Input disabled id="password" size="normal" maxLength={524288} textOverflow="clip" theme="normal" width="initial" dataHook="login-username" onChange={evt => this.setState({username: evt.target.value})}/>
+            </TextField>
+          </div>
+          <div className={s.inputField}>
+            <Checkbox checked={this.state.checked} onChange={() => this.setState({checked: !this.state.checked})}>Remember me</Checkbox>
+          </div>
+          <div className={s.buttomRight}>
+            <Button dataHook="login-btn" onClick={() => this.props.onLoginClick(this.state.username)}>Login</Button>
+          </div>
+          <div className={s.buttomLeft}>
+            <TextLink size="small" link="">Sign up</TextLink>
+          </div>
+        </div>
       </div>
     </div>);
   }
