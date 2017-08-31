a8b59c6096f852212db6df8b419698104dd81856
miri <mirif@wix.com>
2017-08-31 12:38:22 +0300

    added user toolbar component test

--- src/components/UserToolbar/UserToolbar.spec.js
+++ src/components/UserToolbar/UserToolbar.spec.js
@@ -1,3 +1,22 @@
-/**
- * Created by mirif on 30/08/2017.
- */
+import {expect} from 'chai';
+import {mount} from 'enzyme';
+import React from 'react';
+import UserToolbar from './UserToolbar';
+import {textTestkitFactory} from 'wix-style-react/dist/testkit/enzyme';
+
+describe('User Toolbar component test', () => {
+  const render = (props = {}) => (
+    mount(
+      <UserToolbar {...props}/>,
+      {attachTo: document.createElement('div')}
+    )
+  );
+
+  it('should render the correct user name', () => {
+    const propsObj = {
+      username: 'Miri'
+    };
+    const wrapper = render(propsObj);
+    expect(textTestkitFactory({wrapper, dataHook: 'username'}).getText()).to.equal(propsObj.username);
+  });
+});
